a7c4d7332e78b9137379e37095b8df06355618cf
ajabar99 <102982691+ajabar99@users.noreply.github.com>
2022-04-05 11:44:20 -0400

    Add files via upload

---
+++ addmachine.py
@@ -0,0 +1,4 @@
+first_number = int(input('what is the first number you want to add?'))
+second_number = int(input('what is the second number you want to add?'))
+sum = first_number + second_number
+print('the sum of your two numbers are',sum)
